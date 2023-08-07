70987ccc3ba945590f4716ca08a9adda8b2ea905
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-07 20:00:55 +0530

    Chart

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4300,
+  },
+  {
+    name: "Feb",
+    "Active User": 2900,
+  },
+  {
+    name: "Mar",
+    "Active User": 7000,
+  },
+  {
+    name: "Apr",
+    "Active User": 3930,
+  },
+  {
+    name: "May",
+    "Active User": 5090,
+  },
+  {
+    name: "Jun",
+    "Active User": 1100,
+  },
+  {
+    name: "Jul",
+    "Active User": 3330,
+  },
+  {
+    name: "Aug",
+    "Active User": 4390,
+  },
+  {
+    name: "Sep",
+    "Active User": 3000,
+  },
+  {
+    name: "Oct",
+    "Active User": 2000,
+  },
+  {
+    name: "Nov",
+    "Active User": 1000,
+  },
+  {
+    name: "Dec",
+    "Active User": 900,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./home.css";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title={"User Analytics"}
+        grid
+        dataKey={"Active User"}
+      />
     </div>
   );
 }
