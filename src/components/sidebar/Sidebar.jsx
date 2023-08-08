451348c5004685e2bdedba1a9ab36420cc690de6
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-08 18:53:18 +0530

    All work done 😊

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -22,10 +22,12 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyle className="sidebarIcon" />
-              <span className="sidebarIconText">Home</span>
-            </li>
+            <Link to={"/"} className="link">
+              <li className="sidebarListItem active">
+                <LineStyle className="sidebarIcon" />
+                <span className="sidebarIconText">Home</span>
+              </li>
+            </Link>
             <li className="sidebarListItem">
               <Timeline className="sidebarIcon" />
               <span className="sidebarIconText">Analytics</span>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./topbar.css";
 import { Language, NotificationsNone, Settings } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">JollyRoger</span>
+          <Link to={"/"} className="link">
+            <span className="logo">JollyRoger</span>
+          </Link>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
